6bab3256fda78be9c8ecb73586f8d6fcb5b1deed
mark.sauer.utley <mark.sauer.utley@taboola.com>
2019-05-20 13:52:57 -0400

    update babel

--- src/index.test.js
+++ src/index.test.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import { expect } from 'chai'
-import { mount, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import Taboola from './index.js'
+import React from 'react';
+import { expect } from 'chai';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Taboola from './index.js';
 
-configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() });
 
-describe('<Taboola />', () => {
-    it('renders a taboola asset', () => {
-        const wrapper = mount((
-            <Taboola publisher={'taboola-training'} 
-                pageType={'article'} 
-                placement={'Below Article Thumbnails'} 
-                mode={'thumbnails-m'} 
-                targetType={'mix'} 
-            />
-        ));
-        expect(wrapper.find('div').exists()).to.equal(true)
-    })
-})
+describe('Taboola Component', () => {
+	it('renders without crashing', () => {
+		const wrapper = mount(
+			<Taboola
+				currentUrl={'https://www.awesomenews.com/dogs-rule'}
+				publisher={'taboola-training'}
+				pageType={'article'}
+				placement={'Below Article Thumbnails'}
+				mode={'thumbnails-m'}
+				targetType={'mix'}
+			/>
+		);
+		expect(wrapper.find('div').exists()).to.equal(true);
+	});
+});
